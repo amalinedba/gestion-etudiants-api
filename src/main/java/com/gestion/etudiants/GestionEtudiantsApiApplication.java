067b56d0205564956f0cc6645cbebb807b9ba9ee
amalinedba <72950056+amalinedba@users.noreply.github.com>
2023-11-04 09:32:15 -0400

    Intialisation du projet

---
+++ src/main/java/com/gestion/etudiants/GestionEtudiantsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.etudiants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEtudiantsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEtudiantsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/etudiants/GestionEtudiantsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.etudiants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEtudiantsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
